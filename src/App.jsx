f645e61fbb69544a8e91557269c2c14045f83709
gela349 <www.angelaadubea25@gmail.com>
2025-07-15 18:00:52 +0000

    worked on the Sdgs

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
-import SDGs from "./pages/SDGs"; // or ./component/ if it's a section
+
 import Team from "./pages/Team"; // same note as above
 import VisionMission from "./pages/VisionMission"; // same note as above
 import Contact from "./pages/Contact"; // if you have a Contact page
 import All from "./pages/All"; // if you have an All page that includes all sections
+import Sdgs from "./pages/SDGs";
 
 const peellnnova = createBrowserRouter([
   { path: "/", element: <All /> },
   { path: "/about", element: <About /> },
   { path: "/product", element: <Product /> },
-  { path: "/sdgs", element: <SDGs /> },
+  { path: "/sdgs", element: <Sdgs /> },
   { path: "/team", element: <Team /> },
   { path: "/vision-mission", element: <VisionMission /> },
   {path: "/contact", element: <Contact /> },
